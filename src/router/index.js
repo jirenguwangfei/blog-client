e90a5b1ac8d2dbba6828d1f0efe5394bfa8bfdf6
jirenguwangfei <1208321826@qq.com>
2018-09-29 18:23:22 +0800

    再一次提交

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Index from '@/pages/Index/template.vue'
+import My from '@/pages/My/template.vue'
+import Edit from '@/pages/Edit/template.vue'
+import Create from '@/pages/Create/template.vue'
+import User from '@/pages/User/template.vue'
+import Register from '@/pages/Register/template.vue'
+import Detail from '@/pages/Detail/template.vue'
+import Login from '@/pages/Login/template.vue'
 
 Vue.use(Router)
 
@@ -8,8 +15,35 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      component: Index
+    },
+    {
+      path: '/login',
+      component: Login
+    },
+    {
+      path: '/my',
+      component: My
+    },
+    {
+      path: '/edit',
+      component: Edit
+    },
+    {
+      path: '/create',
+      component: Create
+    },
+    {
+      path: '/detail',
+      component: Detail
+    },
+    {
+      path: '/user',
+      component: User
+    },
+    {
+      path: '/register',
+      component: Register
+    },
   ]
 })
