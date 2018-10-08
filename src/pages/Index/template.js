62cbe9981c48a453ec75fdc2adca203a1e36cac1
jirenguwangfei <1208321826@qq.com>
2018-10-08 11:29:10 +0800

    页面的less

--- src/pages/Index/template.js
+++ src/pages/Index/template.js
@@ -1,5 +1,12 @@
+import request from '@/helpers/request.js'
+import auth from '@/api/auth.js'
+import blog from '@/api/blog.js'
+
+window.request = request
+window.auth = auth
+window.blog = blog
+
 export default {
-  name: 'HelloWorld',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
